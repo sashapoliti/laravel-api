6c6e914597965ddb6304a836e2cf2367b4d7aa40
sashapoliti <sas.politi@gmail.com>
2024-06-12 17:27:09 +0200

    add types in projectcontroller api

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -5,13 +5,17 @@ namespace App\Http\Controllers\Api;
 use App\Models\Project;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Contracts\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $projects = Project::with('technologies', 'type')->paginate(4);
+        $type = $request->query('type');
+        $projects = Project::with('technologies', 'type')->when($type, function (Builder $query, string $type) {
+            return $query->where('type_id', $type);
+        })->paginate(4);
         return response()->json([
             'status' => 'success',
             'message' => 'Projects retrieved successfully',
