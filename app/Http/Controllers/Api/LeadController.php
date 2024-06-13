41a1df68bfaabab70b3200e7838e613fcb9f2e4b
sashapoliti <sas.politi@gmail.com>
2024-06-13 10:56:59 +0200

    complete model, controller and fix migration, migrate then in database new table leads

--- app/Http/Controllers/Api/LeadController.php
+++ app/Http/Controllers/Api/LeadController.php
@@ -2,10 +2,24 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Models\Lead;
+use App\Mail\NewContact;
+
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Validator;
+
 
 class LeadController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        $data = $request->all();
+        $lead = new Lead();
+        $lead->fill($data);
+        $lead->save();
+
+        Mail::to('zDj7Z@example.com')->send(new NewContact($lead));
+    }
 }
--- database/migrations/2024_06_13_083954_create_leads_table.php
+++ database/migrations/2024_06_13_083954_create_leads_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('leads', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 255);
+            $table->string('address', 255);
+            $table->text('message');
             $table->timestamps();
         });
     }
