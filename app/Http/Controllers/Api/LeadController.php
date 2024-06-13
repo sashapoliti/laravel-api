32b7611d84cfdeca76e7073052efcb8ca184f62d
sashapoliti <sas.politi@gmail.com>
2024-06-13 14:54:52 +0200

    add validation in lead controller

--- app/Http/Controllers/Api/LeadController.php
+++ app/Http/Controllers/Api/LeadController.php
@@ -16,10 +16,27 @@ class LeadController extends Controller
     public function store(Request $request)
     {
         $data = $request->all();
+        $validator = Validator::make($data, [
+            'name' => 'required',
+            'address' => 'required|email',
+            'message' => 'required'
+        ]);
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 'error',
+                'message' => $validator->errors()
+            ], 400);
+        }
+
         $lead = new Lead();
         $lead->fill($data);
         $lead->save();
 
-        Mail::to('zDj7Z@example.com')->send(new NewContact($lead));
+        Mail::to('admin@boolpress.com')->send(new NewContact($lead));
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Lead created successfully'
+        ], 200);
     }
 }
